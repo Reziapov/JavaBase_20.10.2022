b004fbc263ca98154b3ba6e456a56aed3fda075a
Reziapov Artur <areziapov@gmail.com>
2022-10-20 22:04:34 +0300

    push main

---
+++ src/main/java/com/hillel/reziapov/homeworks/homework2/Main.java
@@ -0,0 +1,10 @@
+package com.hillel.reziapov.homeworks.homework2;
+
+public class Main {
+    public static void main(String[] args) {
+        double n = 27.1745895, e = 78.0419529;
+        System.out.println("Taj Mahal");
+        System.out.println("North: " + n + "\u00B0");
+        System.out.println("East: " + e + "\u00B0");
+    }
+}
---
+++ src/main/java/com/hillel/reziapov/homeworks/homework3/Main.java
@@ -0,0 +1,7 @@
+package com.hillel.reziapov.homeworks.homework3;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
