76e7d723c6578df7f9f739f2f33a11abbfebd379
Reziapov Artur <areziapov@gmail.com>
2022-10-24 21:39:17 +0300

    push main

---
+++ src/main/java/com/hillel/reziapov/homeworks/homework4/Main.java
@@ -0,0 +1,12 @@
+package com.hillel.reziapov.homeworks.homework4;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 5;
+        int c = 4;
+        int volume = a * b * c;
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+
+    }
+}
