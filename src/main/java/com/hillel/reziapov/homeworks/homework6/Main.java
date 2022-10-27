85db80405becdec513774d7481c89b963c7af179
Reziapov Artur <areziapov@gmail.com>
2022-10-27 21:46:47 +0300

    homework 6 (add comment)

---
+++ src/main/java/com/hillel/reziapov/homeworks/homework6/Main.java
@@ -0,0 +1,25 @@
+package com.hillel.reziapov.homeworks.homework6;
+
+public class Main {
+    public static void main(String[] args) {
+/*
+ввести ім'я першої команди
+ввести кількість фрагів для 5-ти гравців першої команди (тобто має бути 5 змінних)
+ввести ім'я другої команди
+ввести кількість фрагів для 5-ти гравців другої команди (тобто має бути 5 змінних)
+підрахувати середнє арифметичне балів кожної команди
+порівняти результати команд та визначити переможця
+вивести у консоль:
+"Перемогла команда" + teamName + "набрала" + result + "очків"
+ */
+
+        /*
+        if (scanner.hasNextInt()) {
+//            int number = scanner.nextInt();
+//            System.out.println(number);
+//        } else {
+//            System.out.println("Wrong data! Restart application");
+//            System.exit(0);
+         */
+    }
+}
