aef011ada28f3e9269e19e697a5097db8960400a
Reziapov Artur <areziapov@gmail.com>
2022-10-29 16:52:06 +0300

    homework 6 (1 part)

--- src/main/java/com/hillel/reziapov/homeworks/homework6/Main.java
+++ src/main/java/com/hillel/reziapov/homeworks/homework6/Main.java
@@ -1,5 +1,7 @@
 package com.hillel.reziapov.homeworks.homework6;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 /*
@@ -13,13 +15,105 @@ public class Main {
 "Перемогла команда" + teamName + "набрала" + result + "очків"
  */
 
-        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ВЕДІТЬ ІМ'Я ПЕРШОЇ КОМАНДИ");
+        String nameFirstcommand = scanner.nextLine();
+
+        System.out.println("Ведіть числове значення фрагів для першого гравця: ");
         if (scanner.hasNextInt()) {
-//            int number = scanner.nextInt();
-//            System.out.println(number);
-//        } else {
-//            System.out.println("Wrong data! Restart application");
-//            System.exit(0);
-         */
+            int firstGamerfirstCommand = scanner.nextInt();
+        } else {
+            System.out.println("Невірний формат значення. Перезавантажте програму");
+            System.exit(0);
+        }
+        scanner.nextLine();
+
+        System.out.println("Ведіть числове значення фрагів для другого гравця: ");
+        if (scanner.hasNextInt()) {
+            int secondGamerfirstCommand = scanner.nextInt();
+        } else {
+            System.out.println("Невірний формат значення. Перезавантажте програму");
+            System.exit(0);
+        }
+        scanner.nextLine();
+
+        System.out.println("Ведіть числове значення фрагів для третього гравця: ");
+        if (scanner.hasNextInt()) {
+            int threeGamerfirstCommand = scanner.nextInt();
+        } else {
+            System.out.println("Невірний формат значення. Перезавантажте програму");
+            System.exit(0);
+        }
+        scanner.nextLine();
+
+        System.out.println("Ведіть числове значення фрагів для четвертого гравця: ");
+        if (scanner.hasNextInt()) {
+            int fourGamerfirstCommand = scanner.nextInt();
+        } else {
+            System.out.println("Невірний формат значення. Перезавантажте програму");
+            System.exit(0);
+        }
+        scanner.nextLine();
+
+        System.out.println("Ведіть числове значення фрагів для п'ятого гравця: ");
+        if (scanner.hasNextInt()) {
+            int fiveGamerfirstCommand = scanner.nextInt();
+        } else {
+            System.out.println("Невірний формат значення. Перезавантажте програму");
+            System.exit(0);
+        }
+        scanner.nextLine();
+
+        System.out.println("ВЕДІТЬ ІМ'Я ДРУГОЇ КОМАНДИ");
+        String nameSecondcommand = scanner.nextLine();
+
+        System.out.println("Ведіть числове значення фрагів для першого гравця: ");
+        if (scanner.hasNextInt()) {
+            int firstGamersecondCommand = scanner.nextInt();
+        } else {
+            System.out.println("Невірний формат значення. Перезавантажте програму");
+            System.exit(0);
+        }
+        scanner.nextLine();
+
+        System.out.println("Ведіть числове значення фрагів для другого гравця: ");
+        if (scanner.hasNextInt()) {
+            int secondGamersecondCommand = scanner.nextInt();
+        } else {
+            System.out.println("Невірний формат значення. Перезавантажте програму");
+            System.exit(0);
+        }
+        scanner.nextLine();
+
+        System.out.println("Ведіть числове значення фрагів для третього гравця: ");
+        if (scanner.hasNextInt()) {
+            int threeGamersecondCommand = scanner.nextInt();
+        } else {
+            System.out.println("Невірний формат значення. Перезавантажте програму");
+            System.exit(0);
+        }
+        scanner.nextLine();
+
+        System.out.println("Ведіть числове значення фрагів для четвертого гравця: ");
+        if (scanner.hasNextInt()) {
+            int fourGamersecondCommand = scanner.nextInt();
+        } else {
+            System.out.println("Невірний формат значення. Перезавантажте програму");
+            System.exit(0);
+        }
+        scanner.nextLine();
+
+        System.out.println("Ведіть числове значення фрагів для п'ятого гравця: ");
+        if (scanner.hasNextInt()) {
+            int fiveGamersecondCommand = scanner.nextInt();
+        } else {
+            System.out.println("Невірний формат значення. Перезавантажте програму");
+            System.exit(0);
+        }
+        scanner.nextLine();
+
+//int sum1 = firstGamerfirstCommand + secondGamerfirstCommand;
+
+        //System.out.println("Перемогла команда: " + sum1);
     }
 }
