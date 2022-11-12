c71002ac7295d0a3bfd283d240d8873fd375a579
Reziapov Artur <areziapov@gmail.com>
2022-11-12 20:50:24 +0200

    homework 9

---
+++ src/main/java/com/hillel/reziapov/homeworks/homework9/Main.java
@@ -0,0 +1,14 @@
+package com.hillel.reziapov.homeworks.homework9;
+
+public class Main {
+    public static void main(String[] args) {
+//        Є дві команди регбі з 25 гравців різного віку у кожній.
+//        Вік беремо випадковим чином в діапазоні від 18 до 40.
+//        Виведіть у двох рядках вік гравців кожної команди.
+//        Порахуйте середній вік гравців кожної команди та виведіть на екран.
+
+        int[] array = new int[25];
+        System.out.println(array);
+
+    }
+}
