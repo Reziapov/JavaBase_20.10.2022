c7c791ee6ee8fed26664cc1691d2fdd535f479f6
Reziapov Artur <areziapov@gmail.com>
2022-11-15 20:10:14 +0200

    homework 9 (version 2)

--- src/main/java/com/hillel/reziapov/homeworks/homework9/Main.java
+++ src/main/java/com/hillel/reziapov/homeworks/homework9/Main.java
@@ -6,38 +6,36 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        int[] team1 = new int[25];
-        int sum1 = 0;
+        final int counter = 25;
 
-        for (int i = 0; i < team1.length; i++) {
+        int[] team1 = new int[counter];
+        int[] team2 = new int[counter];
+
+        double sum1 = 0;
+        double sum2 = 0;
+
+        for (int i = 0; i < counter; i++) {
 
             team1[i] = (int) (18 + (Math.random() * 23));
+            team2[i] = (int) (18 + (Math.random() * 23));
         }
+        for (int i = 0; i < counter; i++) {
+            sum1 += team1[i];
+            sum2 += team2[i];
+        }
+
         System.out.println("_ПЕРША КОМАНДА_");
         System.out.println(Arrays.toString(team1));
 
-        for (int i = 0; i < team1.length; i++) {
-            sum1 += team1[i];
-        }
-        sum1 /= team1.length;
+        sum1 /= counter;
 
         System.out.println("Середній вік гравців: " + sum1);
         System.out.println();
 
-        int[] team2 = new int[25];
-        int sum2 = 0;
-
-        for (int i = 0; i < team2.length; i++) {
-
-            team2[i] = (int) (18 + (Math.random() * 23));
-        }
         System.out.println("_ДРУГА КОМАНДА_");
         System.out.println(Arrays.toString(team2));
 
-        for (int i = 0; i < team2.length; i++) {
-            sum2 += team2[i];
-        }
-        sum2 /= team2.length;
+        sum2 /= counter;
 
         System.out.println("Середній вік гравців: " + sum2);
     }
