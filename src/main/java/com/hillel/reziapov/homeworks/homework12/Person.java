545a3dbad2fe4119262030da945168d0a2e01413
Reziapov Artur <areziapov@gmail.com>
2022-11-22 19:40:50 +0200

    homework 12 (final)

--- src/main/java/com/hillel/reziapov/homeworks/homework12/Person.java
+++ src/main/java/com/hillel/reziapov/homeworks/homework12/Person.java
@@ -1,22 +1,19 @@
 package com.hillel.reziapov.homeworks.homework12;
 
+
 public class Person {
 
     public static void main(String[] args) {
-
-
-        personInfo("Will Smith", "New York", "2936729462846");
-        personInfo("Jackie Chan", "Shanghai", "12312412412");
-        personInfo("Sherlock Holmes", "London", "37742123513");
+        String print1 = personInfo("Will", "Smith", "New York", "2936729462846");
+        String print2 = personInfo("Jackie", "Chan", "Shanghai", "12312412412");
+        String print3 = personInfo("Sherlock", "Holmes", "London", "37742123513");
+        System.out.println(print1 + "\n" + print2 + "\n" + print3);
 
     }
+    static String personInfo(String name, String lastName, String city, String number) {
+        return "Зателефонувати громадянину " +
+                name + " " + lastName + " із міста " + city + " можна за номером " + number;
 
-    static String personInfo(String name, String city, String number) {
-
-
-        System.out.println("Зателефонувати громадянину " + name + " з міста " + city + " за номером " + number);
-
-        return name + city + number;
     }
 }
 
