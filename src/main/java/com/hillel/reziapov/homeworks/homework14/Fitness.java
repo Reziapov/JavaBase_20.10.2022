ef9e13a690f8f3150735a5d13f2eaeb59872dedc
Reziapov Artur <areziapov@gmail.com>
2022-12-07 16:02:30 +0200

    homework 14 (Fitness)

--- src/main/java/com/hillel/reziapov/homeworks/homework14/Fitness.java
+++ src/main/java/com/hillel/reziapov/homeworks/homework14/Fitness.java
@@ -3,16 +3,17 @@ package com.hillel.reziapov.homeworks.homework14;
 public class Fitness {
 
     private final String name;
-    String lastname;
+    private String lastname;
     private final int number;
     private final int month;
-    private int year;
+    private final int year;
     private final String email;
     private final long phone;
-    double weigh;
-    String pressure;
-    int steps;
+    private double weigh;
+    private String pressure;
+    private int steps;
     private int a = 2020;
+    private int age;
 
     public Fitness(String name, String lastname, int number, int month, int year, String email, long phone,
                    double weigh, String pressure, int steps) {
@@ -26,18 +27,57 @@ public class Fitness {
         this.weigh = weigh;
         this.pressure = pressure;
         this.steps = steps;
+        this.age = a - year;
     }
 
-    int age = a - year;
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setLastname() {
+        this.lastname = lastname;
+    }
+
+    public void setWeigh() {
+        this.weigh = weigh;
+    }
+
+    public void setPressure() {
+        this.pressure = pressure;
+    }
+
+    public void setSteps() {
+        this.steps = steps;
+    }
 
     public int getAge() {
-        age = a - year;
         return age;
     }
 
     void printAccountInfo() {
-        System.out.println(name + " " + lastname + " Age:" + getAge() + " " + "(" + number + "." +
-                month + "." + year + ") email: " + email + "; " + "phone: " + phone + "; weigh: " +
-                weigh + "; " + " pressure: " + pressure + "; steps: " + steps);
+        System.out.println(getName() + " " + lastname + " Age:" + getAge() + " (" + getNumber() + "." + getMonth()
+                + "." + getYear() + ") email: " + getEmail() + "; " + "phone: " + getPhone() + "; weigh: " + weigh
+                + "" + " pressure: " + pressure + "; steps: " + steps);
     }
 }
+
