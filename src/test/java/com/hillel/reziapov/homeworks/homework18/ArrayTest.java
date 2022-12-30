581f5d124462a24e8757eb66e1eda567b41bd3a5
Reziapov Artur <areziapov@gmail.com>
2022-12-30 18:22:04 +0200

    Homework 18 (Array)

--- src/test/java/com/hillel/reziapov/homeworks/homework18/ArrayTest.java
+++ src/test/java/com/hillel/reziapov/homeworks/homework18/ArrayTest.java
@@ -1,16 +1,48 @@
 package com.hillel.reziapov.homeworks.homework18;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-
 public class ArrayTest {
+    static int[] arrayTest;
+
+    @BeforeAll
+    static void beforeAll() {
+        arrayTest = new int[]{4, 4, 4, 4};
+    }
 
     @Test
-    public void test() {
-        Array.dimensionalMatrix();
-
+    public void test1() {
+        assertFalse(Array.arraySquare(null));
     }
+
+    @Test
+    public void test2() {
+        assertFalse(Array.arraySquare(new int[0][0]));
+    }
+
+    @Test
+    public void test3() {
+        assertTrue(Array.arraySquare(new int[4][4]));
+    }
+
+    @Test
+    public void test4() {
+        double average = Array.arrayMean(new int[]{});
+        assertEquals(0.0, average);
+    }
+
+    @Test
+    public void test5() {
+        double average = Array.arrayMean(null);
+        assertEquals(0.0, average);
+    }
+
+    @Test
+    public void test6() {
+        double average = Array.arrayMean(arrayTest);
+        assertEquals(4.0, average);
+    }
+
 }
-
-
